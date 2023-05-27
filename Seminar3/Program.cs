dba73e227da9bbe5c3b2efb3de35048fc422870a
Artem Anatolievich <katod1978@yandex.ru>
2023-05-27 16:48:39 +0700

    Seminar3.3

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-/*Console.WriteLine("Введите число");
+Console.WriteLine("Введите число");
 string text = Console.ReadLine();
 int length =text.Length;
 int num = Convert.ToInt32(text);
@@ -22,25 +22,38 @@ if (length > 2){
     if (length == 2 && num/10 != num%10) 
         isPal = -1;
 }
- Console.WriteLine(isPal == 1? text + " -> да":text + " -> нет"); */
-    
-    
+ Console.WriteLine(isPal == 1? text + " -> да":text + " -> нет"); 
+        
 // Для добуквенного исполнения задания нужнa проверкa на пятизначность. Но в данной версии
 // проверяет любое число на палиндромность. Проще решить можно было через символы.
 
 
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-
-
+Console.WriteLine("Введите x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2));
+Console.WriteLine ("Растояние между точквми - " + distance);
 
 
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-/*Console.WriteLine("Введите число");
+
+Console.WriteLine("Введите число");
 int N = Convert.ToInt32(Console.ReadLine());
 Console.Write(N + " -> ");
 for (int i = 1;i <= N;i++)
     Console.Write(i < N? Math.Pow(i,3) + ",":Math.Pow(i,3));
-Console.WriteLine();*/
+Console.WriteLine();
 
 
 
